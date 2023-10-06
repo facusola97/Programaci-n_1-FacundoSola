f659505243858ede4ee6d36431870a1c01be5077
facusola97 <114526665+facusola97@users.noreply.github.com>
2023-10-06 18:48:45 -0300

    nuevoproyecto

---
+++ Ejercicio Entregable - Entidad Financiera/Back/BancoDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Back.Clases;
+using Microsoft.EntityFrameworkCore;
+
+namespace Back
+{
+    public class BancoDbContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<CuentaBancaria> CuentasBancarias { get; set; }
+        public DbSet<TarjetaDeCredito> TarjetasDeCredito { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-BAI14A1;database=BDFinaciero;trusted_connection=true;Encrypt=False");
+        }
+    }
+
+
+
+
+}
---
+++ Ejercicio Entregable - Entidad Financiera/Back/Clases/Cliente.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.ConstrainedExecution;
+
+namespace Back.Clases
+{
+    public class Cliente
+    {
+        
+       
+        
+       [Key] public int IdCliente { get; set; }
+
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public int dni { get; set; }
+
+        public Cliente(int ClienteId ,string nombre, string apellido, int dni)
+        {
+            IdCliente = ClienteId;
+            Nombre = nombre;
+            Apellido = apellido;
+            this.dni = dni;
+        }
+        public Cliente(string nombre, string apellido, int dni)
+        {
+            // Supongamos que aquí generamos un IdCliente único, por ejemplo, utilizando algún método de generación de ID único.
+           
+            Nombre = nombre;
+            Apellido = apellido;
+            this.dni = dni;
+        }
+
+
+        
+
+
+
+
+
+
+
+
+    }
+}
